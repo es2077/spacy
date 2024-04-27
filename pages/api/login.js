087bd296e02336d8eaf24072f2408ddbea593b79
Gabriel Rubens Abreu <grsabreu@gmail.com>
2024-04-27 08:48:33 -0300

    add basic auth system (#41)

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import cookie from "cookie";
 
 export default async function (req, res) {
   try {
     const { password, email } = req.body.input.input;
-    const result = await fetch(process.env.HASURA_GRAPHQL_API, {
+    const result = await fetch(process.env.NEXT_PUBLIC_HASURA_GRAPHQL_API, {
       method: "POST",
       headers: {
         "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
@@ -55,11 +56,15 @@ export default async function (req, res) {
         }
       );
 
+      const authCookie = cookie.serialize("spacy_auth", token, {
+        domain: "localhost", // Set domain to localhost
+        path: "/",
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7, // 1 week
+      });
+
       // set cookies
-      res.setHeader(
-        "Set-Cookie",
-        `spacy_auth=${token}; Secure; HttpOnly; Path=/;`
-      );
+      res.setHeader("Set-Cookie", authCookie);
       res.status(200).json({
         token,
         error: null,
